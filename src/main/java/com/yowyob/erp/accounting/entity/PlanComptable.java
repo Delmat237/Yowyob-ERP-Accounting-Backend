ed7257592abc346192d4f5a6b536a763fa9ff7d1
Delmat Leonel AZANGUE <142000059+Delmat237@users.noreply.github.com>
2025-08-07 20:40:51 +0100

    Update PlanComptable.java

--- src/main/java/com/yowyob/erp/accounting/entity/PlanComptable.java
+++ src/main/java/com/yowyob/erp/accounting/entity/PlanComptable.java
@@ -1,5 +1,6 @@
 package com.yowyob.erp.accounting.entity;
 
+import com.yowyob.erp.common.entity.Auditable;
 import jakarta.validation.constraints.*;
 import lombok.Data;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
@@ -16,7 +17,7 @@ import static org.springframework.data.cassandra.core.cql.PrimaryKeyType.PARTITI
 
 @Table("plan_comptable")
 @Data
-public class PlanComptable {
+public class PlanComptable implements Auditable {
 
     @PrimaryKey
     private PlanComptableKey key;
@@ -38,6 +39,25 @@ public class PlanComptable {
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
+
+    @Size(max = 255, message = "Créé par ne doit pas dépasser 255 caractères")
+    private String createdBy;
+
+    @Size(max = 255, message = "Mis à jour par ne doit pas dépasser 255 caractères")
+    private String updatedBy;
+
+    @Override
+    public String getTenantId() {
+        return key.getTenantId();
+    }
+
+    @Override
+    public void setTenantId(String tenantId) {
+        if (key == null) {
+            key = new PlanComptableKey();
+        }
+        key.setTenantId(tenantId);
+    }
 }
 
 @PrimaryKeyClass
